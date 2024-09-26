8bf89b0e6f9b35b7dfa380f0ba68de51c7bfa0a0
Alberto Battistel <alberto.battistel@hs-furtwangen.de>
2024-09-26 13:20:25 +0200

    plot B fields on planes
    it is wierd, I am not sure it is correct
    good luck

---
+++ +helpers/calc_B_at_points.m
@@ -0,0 +1,20 @@
+function [B] = calc_B_at_points(B_positions, elem_centers, e_curr, elem_areas)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+mu0 = 4*pi*1e-7;
+B = zeros(length(B_positions), 3);
+
+for i_point = 1:length(B_positions)
+    point = B_positions(i_point,:);
+    r = point - elem_centers;
+    
+    % if need to check for zero division
+    r_mag = vecnorm(r.',2,1).';
+    l_non_zero = find(r_mag);
+
+    dB = mu0/(4*pi)*cross(e_curr(l_non_zero,:).*elem_areas(l_non_zero,:), r(l_non_zero,:))./norm(r(l_non_zero,:).^3); % magnetic flux density, in T
+
+    B(i_point, :) = sum(dB,1);
+end
+
+end
