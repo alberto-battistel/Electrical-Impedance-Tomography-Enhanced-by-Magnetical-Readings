2e15ef5cc4459641bbab13b124eb2bdd2132e764
Alberto Battistel <alberto.battistel@hs-furtwangen.de>
2024-10-22 10:16:43 +0200

    calc_b_at_points recycles j*elem_vol

--- +helpers/calc_B_at_points.m
+++ +helpers/calc_B_at_points.m
@@ -4,6 +4,9 @@ function [B] = calc_B_at_points(B_positions, elem_centers, e_curr, elem_vol)
 mu0 = 4*pi*1e-7;
 B = zeros(length(B_positions), 3);
 
+% precalculate current density * element volume
+j_times_vol = e_curr.*elem_vol;
+
 for i_point = 1:length(B_positions)
     point = B_positions(i_point,:);
     r = point - elem_centers;
@@ -12,7 +15,7 @@ for i_point = 1:length(B_positions)
     r_mag = vecnorm(r.',2,1).';
     l_non_zero = find(r_mag);
 
-    dB = cross(e_curr(l_non_zero,:).*elem_vol(l_non_zero,:), r(l_non_zero,:))./norm(r(l_non_zero,:).^3); % magnetic flux density, in T
+    dB = cross(j_times_vol(l_non_zero,:), r(l_non_zero,:))./norm(r(l_non_zero,:).^3); % magnetic flux density, in T
 
     B(i_point, :) = mu0/(4*pi)*sum(dB,1);
 end
