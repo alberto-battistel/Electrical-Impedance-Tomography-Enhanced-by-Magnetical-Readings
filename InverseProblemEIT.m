cd0a5a61893b1530f2925bda361eba971642ac80
Alberto Battistel <alberto.battistel@hs-furtwangen.de>
2024-12-09 08:41:50 +0100

    calculate jacobian

--- InverseProblemEIT.m
+++ InverseProblemEIT.m
@@ -1,4 +1,4 @@
-classdef InverseProblemEIT< handle
+classdef InverseProblemEIT< matlab.mixin.Copyable
     %UNTITLED2 Summary of this class goes here
     %   Detailed explanation goes here
 
@@ -10,12 +10,17 @@ classdef InverseProblemEIT< handle
         cheb_coeffs
         zern_set
         cheb_set
-        values
+        cond_values
         coeff_matrix
         img
         img_0
-        elem_curr
+        elem_currents_0
+        elem_currents
         phantom
+        unnormalized_coil_voltages
+        pert_amplitude
+        coil_system
+        unnormalized_coil_voltages_0
     end
 
     properties(Hidden)
@@ -65,18 +70,19 @@ classdef InverseProblemEIT< handle
             obj.cheb_set = cos(acos(obj.scaled_elem_centers(:,3)).*obj.cheb_coeffs);
         end
 
-        function calc_values(obj)
+        function calc_cond_values(obj, pert_amplitude)
+            obj.pert_amplitude = pert_amplitude;
             n_cheb = size(obj.cheb_set,2);
             n_zern = size(obj.zern_set,2);
             
-            obj.values = zeros(length(obj.elem_centers), ...
-                n_zern*n_cheb);
+            obj.cond_values = zeros(length(obj.elem_centers), n_zern*n_cheb);
             obj.coeff_matrix = zeros(3, n_zern*n_cheb);
             for i_cheb = 1:n_cheb
                 for i_zern = 1:n_zern
                     idx = (i_cheb-1)*n_zern + i_zern;
                     obj.coeff_matrix(:,idx) = [obj.zern_coeffs(i_zern,:), obj.cheb_coeffs(i_cheb)];
-                    obj.values(:,idx) = obj.cheb_set(:,i_cheb).*obj.zern_set(:,i_zern);
+                    obj.cond_values(:,idx) = obj.img_0.elem_data + ...
+                        obj.pert_amplitude*obj.cheb_set(:,i_cheb).*obj.zern_set(:,i_zern);
                 end
             end
         end
@@ -95,20 +101,32 @@ classdef InverseProblemEIT< handle
         end
 
         function assign_values_from_idx(obj, idx)
-            obj.img.elem_data = obj.values(:,idx);
-            obj.EIT.img.elem_data = obj.values(:,idx);
+            if idx ~= 0
+                obj.img.elem_data = obj.cond_values(:,idx);
+                obj.EIT.img.elem_data = obj.cond_values(:,idx);
+            else
+                obj.img.elem_data = obj.img_0.elem_data;
+                obj.EIT.img.elem_data = obj.img_0.elem_data;
+            end
         end
 
         function calc_all_currents(obj)
-
-            obj.elem_curr = zeros(length(obj.elem_centers), 3, obj.phantom.n_elec, size(obj.values,2));
+            obj.elem_currents = zeros(length(obj.elem_centers), 3, obj.phantom.n_elec, size(obj.cond_values,2));
             
-            f = waitbar(0,'Calculating currents...');
-            for ii = 1:size(obj.values,2)
-                obj.elem_curr(:,:,:,ii) = calc_elem_current(obj, ii);
-                waitbar(ii/size(obj.values,2),f);        
+            disp('Calculating currents for each perturbation...')
+            f = waitbar(0,'Calculating currents for each perturbation...');
+            tic;
+            for ii = 0:size(obj.cond_values,2)
+                if ii == 0
+                    obj.elem_currents_0 = obj.calc_elem_current(ii);
+                    continue
+                end
+                obj.elem_currents(:,:,:,ii) = obj.calc_elem_current(ii);
+                waitbar(ii/size(obj.cond_values,2),f);        
             end
             close(f)
+            t = duration(0,0,toc, 'Format', 'hh:mm:ss');
+            fprintf('It took %s\n', t)
         end
 
         function elem_curr = calc_elem_current(obj, idx)
@@ -116,6 +134,58 @@ classdef InverseProblemEIT< handle
             obj.EIT.calc_elem_current()
             elem_curr = obj.EIT.elem_curr;
         end
+
+        function attach_coil_system(obj, coil_system)
+
+            obj.coil_system = coil_system;
+
+        end
+
+        function [unnormalized_coil_voltages_0, unnormalized_coil_voltages] = calc_coil_integrals(obj)
+            model = struct('elem_centers', [], ...
+                           'elem_volumes', [], ...
+                           'elem_curr', []);
+            model.elem_centers = obj.elem_centers;
+            model.elem_volumes = obj.elem_volumes;
+            
+            disp('Calculating integrals for each perturbation...')
+            f = waitbar(0,'Calculating integrals for each perturbation...');
+            unnormalized_coil_voltages = zeros(obj.EIT.n_elec^2,size(obj.cond_values,2));
+            
+            tic;
+            for ii = 0:size(obj.cond_values,2)
+                if ii == 0
+                    model.elem_curr = obj.elem_currents_0;
+                    unnormalized_coil_voltages_0 = reshape(obj.coil_system.calc_coil_integrals( ...
+                                    model, 1:obj.EIT.n_elec), [], 1);
+                    continue
+                end
+                model.elem_curr = obj.elem_currents(:,:,:,ii);
+                unnormalized_coil_voltages(:,ii) = reshape(obj.coil_system.calc_coil_integrals( ...
+                                    model, 1:obj.EIT.n_elec), [], 1);
+                waitbar(ii/size(obj.cond_values,2),f);
+            end
+            close(f)
+            t = duration(0,0,toc, 'Format', 'hh:mm:ss');
+            fprintf('It took %s\n', t)
+
+            obj.unnormalized_coil_voltages = unnormalized_coil_voltages;
+            obj.unnormalized_coil_voltages_0 = unnormalized_coil_voltages_0;
+        end
+
+        function jacobian = calc_jacobian(obj)
+            if isempty(obj.unnormalized_coil_voltages)
+                obj.calc_coil_integrals();
+            end
+
+            d_unnormalized_voltages = obj.unnormalized_coil_voltages-obj.unnormalized_coil_voltages_0;
+            jacobian = d_unnormalized_voltages/obj.pert_amplitude;
+
+
+        end
+
+
+
 
     end
 end
--- inv_problem_test.m
+++ inv_problem_test.m
@@ -10,8 +10,8 @@ phantom.elec_radius = 0.005;
 phantom.radius = 0.1;
 phantom.height = 1.5*phantom.radius;
 phantom.elec_vert_position = phantom.height/2;
-phantom.max_el_sz = 0.004;
-phantom.maxsz = 0.01;
+phantom.max_el_sz = 0.005;
+phantom.maxsz = 0.015;
 phantom.background = 0.503; % muscle at 1 MHz
 phantom.extra_format = @(radius, center){'ball', ...
     sprintf('solid ball = sphere(%.4f,%.4f,%.4f;%.4f);', center(1), center(2), center(3), radius)};
@@ -19,9 +19,10 @@ phantom.ball = 0.136; % inflated lung at 1 MHz
 
 current_ampl = 10e-3;
 freq = 1e6;
+pert_amplitude = 1e-6;
 
-zern_coeffs = helpers.calc_zern_coeffs(4);
-cheb_coeffs = 0:3;
+zern_coeffs = helpers.calc_zern_coeffs(16);
+cheb_coeffs = 0:6;
 
 inv_model = InverseProblemEIT(phantom, current_ampl, zern_coeffs, cheb_coeffs);
 
@@ -29,7 +30,7 @@ inv_model = InverseProblemEIT(phantom, current_ampl, zern_coeffs, cheb_coeffs);
 inv_model.make_zern_set()
 inv_model.make_cheb_set()
 
-inv_model.calc_values()
+inv_model.calc_cond_values(pert_amplitude)
 
 % inv_model.assign_values([8,-8], 5);
 % 
@@ -38,3 +39,31 @@ inv_model.calc_values()
 
 %%
 inv_model.calc_all_currents()
+
+
+
+%% vertical perpendicolar coils
+coil_detectors_radius = 0.005; % m
+coil_detectors.name = 'Vertical perpendicolar coils';
+coil_detectors.radius = coil_detectors_radius; % m
+coil_detectors.orientation = [pi/2,0, 0];
+coil_detectors.center = [phantom.radius + 0.01 + coil_detectors_radius, 0, phantom.elec_vert_position];
+
+mother_coil = Coil(coil_detectors.center, ...
+                   coil_detectors.radius, ...
+                   coil_detectors.orientation);
+coil_system = CoilSystem(mother_coil, phantom.n_elec);
+
+%%
+
+inv_model.attach_coil_system(coil_system)
+unnormalized_coil_voltages = inv_model.calc_coil_integrals;
+% 
+% magnetic_voltages = 2*pi*freq*unnormalized_coil_voltages;
+
+jacobian = inv_model.calc_jacobian();
+
+imagesc(log10(abs(jacobian)))
+
+% coeff_matrix = inv_model.coeff_matrix;
+% save('jacobian', 'jacobian', 'phantom', 'coeff_matrix')
